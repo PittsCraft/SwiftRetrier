4edab1b4eb8d77bd84ce24248c053720c776175a
Pierre Mardon <pierre.mardon@gmail.com>
2023-07-23 22:26:16 +0200

    Add simple flag to enable SwiftLint plugin locally

--- Package.swift
+++ Package.swift
@@ -3,24 +3,37 @@
 
 import PackageDescription
 
+let lint = false
+
+var dependencies: [Package.Dependency] = []
+var plugins: [Target.PluginUsage] = []
+if lint {
+    dependencies = [.package(url: "https://github.com/realm/SwiftLint", exact: "0.52.4")]
+    plugins = [.plugin(name: "SwiftLintPlugin", package: "SwiftLint")]
+}
+
 let package = Package(
     name: "SwiftRetrier",
     platforms: [
         .iOS(.v14),
-        .macOS(.v11)
+        .macOS(.v12)
     ],
     products: [
         .library(
             name: "SwiftRetrier",
             targets: ["SwiftRetrier"]),
     ],
-    dependencies: [],
+    dependencies: dependencies,
     targets: [
         .target(
             name: "SwiftRetrier",
-            dependencies: []),
+            dependencies: [],
+            plugins: plugins
+        ),
         .testTarget(
             name: "SwiftRetrierTests",
-            dependencies: ["SwiftRetrier"]),
+            dependencies: ["SwiftRetrier"],
+            plugins: plugins
+        ),
     ]
 )
